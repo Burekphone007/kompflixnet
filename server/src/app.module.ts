83d5241b7a68a8845c2223cf45740ff4f6a82b9f
Burekphone007 <gorogistvan95@gmail.com>
2021-03-15 21:35:52 +0100

    test module to connect to mongodb(docker)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
